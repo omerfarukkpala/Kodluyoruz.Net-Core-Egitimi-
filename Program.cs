cd9579ded7ee8617a835f3af2b03d984121e70ec
omerfarukkpala <omerfarukkpala@gmail.com>
2022-03-23 14:34:47 +0300

    Hello Word

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace console_programlama
+{
+
+    class Program
+    {
+        public static void Main(string[]args)
+        {
+            Console.WriteLine("Hello, World!"); // " " İçindeki metni console'a yazdırır.
+           
+        
+                                
+        }
+    }
+}
