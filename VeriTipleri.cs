d9e5c3e58df3bb543ed14568ad88e904b73835ab
omerfarukkpala <omerfarukkpala@gmail.com>
2022-03-23 14:59:46 +0300

    isim girişi sağlandı

---
+++ VeriTipleri.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace console_programlama
+{
+
+    class Program
+    {
+        public static void Main(string[]args)
+        {
+            Console.WriteLine("Hello, World!");                      // " " İçindeki metni console'a yazdırır.
+            Console.WriteLine("İsminizi Giriniz:");
+            string name= Console.ReadLine();                        // name değişkenini console'a yazılan bilgiden okur
+                                                                    // ve okuduğunu name değişkenine atar 
+            Console.WriteLine("soyadınızı giriniz:");
+            string surname= Console.ReadLine();                     // surname değişkenini console'a yazılan bilgiden okur
+                                                                    // ve okuduğunu surname değişkenine atar
+            Console.WriteLine("merhaba " + name + " " + surname);   // console'da aldığı bilgileri yandaki şekilde bir kez daha console'a yazdırır
+
+
+   
+        
+                                               
+        }
+    }
+}
