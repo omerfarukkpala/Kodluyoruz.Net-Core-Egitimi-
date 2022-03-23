4c030bf96ffd5f28cefb88cfd21820fb6a45a28f
omerfarukkpala <omerfarukkpala@gmail.com>
2022-03-23 15:13:35 +0300

    Main Methodu - Namespace kavramı - System Using Direktifleri - Run tammalandı

--- VeriTipleri.cs
+++ VeriTipleri.cs
@@ -7,14 +7,14 @@ namespace console_programlama
     {
         public static void Main(string[]args)
         {
-            Console.WriteLine("Hello, World!");                      // " " İçindeki metni console'a yazdırır.
+            Console.WriteLine("Hello, World!");  // " " İçindeki metni console'a yazdırır.
             Console.WriteLine("İsminizi Giriniz:");
-            string name= Console.ReadLine();                        // name değişkenini console'a yazılan bilgiden okur
-                                                                    // ve okuduğunu name değişkenine atar 
+            string name= Console.ReadLine(); // name değişkenini console'a yazılan bilgiden okur
+             // ve okuduğunu name değişkenine atar 
             Console.WriteLine("soyadınızı giriniz:");
-            string surname= Console.ReadLine();                     // surname değişkenini console'a yazılan bilgiden okur
-                                                                    // ve okuduğunu surname değişkenine atar
-            Console.WriteLine("merhaba " + name + " " + surname);   // console'da aldığı bilgileri yandaki şekilde bir kez daha console'a yazdırır
+            string surname= Console.ReadLine(); // surname değişkenini console'a yazılan bilgiden okur
+             // ve okuduğunu surname değişkenine atar
+            Console.WriteLine("merhaba " + name + " " + surname);// console'da aldığı bilgileri yandaki şekilde bir kez daha console'a yazdırır
 
 
    
