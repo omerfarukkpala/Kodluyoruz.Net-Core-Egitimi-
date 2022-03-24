771e32db9ef972b8d1e3066d31b9ea0a2af4b5f0
omerfarukkpala <omerfarukkpala@gmail.com>
2022-03-24 11:52:47 +0300

    for-break-continue alıştırması başladı

---
+++ for-break-continue.cs
@@ -0,0 +1,16 @@
+﻿// Console'da verilen sayıya kadar olan tek sayıları yazdır.
+
+Console.WriteLine("Kaça kadar tek sayıları yazdırmak istiyorsunuz?");       //istem
+
+
+    // int okual = Console.ReadLine();                  // Console'da string ifadeler okunur, alınır.
+    int okual = int.Parse(Console.ReadLine());          // Parse edilmesi gerek.
+
+
+    for (int i = 1; i <= okual; i++)
+    {
+        if(i%2 == 1)
+            Console.WriteLine(i);
+    }
+
+
