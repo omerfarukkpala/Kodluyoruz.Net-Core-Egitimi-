b6f939ad1c99a7501908a204d179bb6896c40a4c
omerfarukkpala <omerfarukkpala@gmail.com>
2022-03-24 11:16:56 +0300

    Karar yapıları-if-elself-ternary-if uygulandı.

---
+++ If(Karar Yapıları)&Ternary.cs
@@ -0,0 +1,18 @@
+﻿int time = DateTime.Now.Hour;               // saati aldık
+
+
+if (time >= 6 && time < 10)                 // 06:00 ve 12:59 arası ise
+    Console.WriteLine("Günaydın!");
+
+else if (time<= 18)
+    Console.WriteLine("İyi Günler!");
+
+else
+    Console.WriteLine("İyi Geceler!");
+
+
+string demo1 = time<=18 ? "İyi Günler!" : "İyi Geceler!";
+    Console.WriteLine(demo1);
+
+string demo2 = time >= 12 && time < 14 ? "İyi öğlenler!" : time <= 18 ? "İyi Günler!" : "İyi Geceler!";
+    Console.WriteLine(demo2);
