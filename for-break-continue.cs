f79c5be1d7fa1d89c19d1a6d8d67eba53e1c7a40
omerfarukkpala <omerfarukkpala@gmail.com>
2022-03-24 12:20:54 +0300

    while - eklendi

--- for-break-continue.cs
+++ for-break-continue.cs
@@ -13,4 +13,45 @@ Console.WriteLine("Kaça kadar tek sayıları yazdırmak istiyorsunuz?");       
             Console.WriteLine(i);
     }
 
+    // 1 ile 1000 arasındaki tek ve çift sayıların kendi içlerindeki toplamını Console'a yazdır.
+
+int tekToplam = 0;          int ciftToplam = 0;
+
+for (int f = 1; f <= 1000; f++)
+{
+    if(f%2 == 1)
+         tekToplam += f;
+    else
+        ciftToplam += f;
+}
+    Console.WriteLine("tek sayıların toplamı: " + tekToplam);
+    Console.WriteLine("çift sayıların toplamı: " + ciftToplam);
+
+
+// break, continue
+
+// Not: İç içe döngüler kullanıldığında break ifadesi yalnızca içinde bulunduğu ilk döngüyü sonlandırır.
+
+
+for (int n = 0; n <= 10; n++)
+{
+    if(n == 4)
+        break; // 4 olunca döngüyü sonlandır.      
+    Console.WriteLine(n); // 0, 1, 2, 3.  
+}
+
+for (int q = 1; q <= 7; q++)
+{
+    if(q == 4)
+        continue; // 4'ü atlar! Döngü bir sonraki iterasyondan devam eder.      
+    Console.WriteLine(q); // 1, 2, 3, 5, 6, 7.
+}
+
+for (int i = 1; i <= 10; i++)
+{
+    if (i % 2 == 0) // i çift sayıya eşit olduğunda döngünün o anki iterasyon sonlanacaktır.
+        continue; // Bu durumda console'a 1 ile 10 arasındaki tek sayılar yazdırılacaktır.
+    Console.WriteLine(i);
+}
+
 
---
+++ while-foreach.cs
@@ -0,0 +1,40 @@
+﻿// while
+
+    // 1'den başlayarak kendisi dahil olmak üzere Console'dan girilen sayıya kadar olan
+    // sayıların aritmetik ortlamasını hesaplayan program
+
+Console.Write("Lütfen bir sayı giriniz: ");
+int okual = int.Parse(Console.ReadLine());
+int demo = 1;      // 1'den başlasın istiyoruz.
+int toplam = 0;
+
+while (demo <= okual)           // demo < okual olsaydı     toplam/okual-1      yazardık.
+{
+    toplam+= demo;
+    demo ++;         
+}
+Console.WriteLine(toplam/okual);
+
+
+    // a'dan z'ye bütün harfleri Console'a yazdır.
+
+char character = 'e';
+
+while (character <= 'z')
+{
+    Console.Write(character);
+    character++;
+}
+
+
+// ForEach
+
+                //     foreach(değişkenTipi değişkenAdı in diziAdı)     { //Komutlar}
+
+string[] cars = {"Seat", "Ford", "BMW", "Cadillac", "Audi", "Bentley"};
+
+foreach (var car in cars)
+{
+    Console.WriteLine(car);
+
+}
